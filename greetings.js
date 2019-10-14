37580f4c1c34e9fd850f1881f66b94d6d0e32b24
Divek john <divek.jes@gmail.com>
2019-10-14 10:56:18 +0530

    program(exporttest)

---
+++ greetings.js
@@ -0,0 +1,12 @@
+//var exports = module.exports = {};
+
+/*exports.sayHelloInEnglish = function() {
+  return "HELLO";
+};
+
+exports.sayHelloInSpanish = function() {
+  return "Hola";
+};
+*/
+module.exports.name = ["Divek","John"];
+
